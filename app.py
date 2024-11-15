b2ec4ec60eab7dc98eea964aa66b1975d14ceda7
sadokkhemila <60410473+sadokkhemila@users.noreply.github.com>
2024-11-15 18:43:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def hello():
     return html.format(nom=os.getenv("NOM", "youtube"), hostname=socket.gethostname(), visites=visites)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8085)
